daeb541d7e73f377955c16ba4ec36bffddbd5b7b
Developer <m_mannaa2008@yahoo.com>
2020-09-26 13:00:47 +0300

    Go to Single View from main views screen

--- project/urls.py
+++ project/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('units/', include('units.urls')),
+    path('units/', include('units.urls' , namespace='unit')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- units/urls.py
+++ units/urls.py
@@ -2,8 +2,9 @@
 from django.urls import path
 from units.views import all_units , single_unit
 
+app_name='units'
 
 urlpatterns = [
     path('', all_units),
-    path('<int:id>', single_unit)
+    path('<int:id>', single_unit, name='unit_detail')
 ]
