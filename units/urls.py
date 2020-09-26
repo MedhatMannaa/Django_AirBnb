2f77fc426da9cd12794da3f4ba5eb44a52ceca5c
Developer <m_mannaa2008@yahoo.com>
2020-09-26 13:33:38 +0300

    Go Single View from main views screen and GoBack Using Links

--- units/urls.py
+++ units/urls.py
@@ -5,6 +5,6 @@ from units.views import all_units , single_unit
 app_name='units'
 
 urlpatterns = [
-    path('', all_units),
+    path('', all_units, name='unit_list'),
     path('<int:id>', single_unit, name='unit_detail')
 ]
